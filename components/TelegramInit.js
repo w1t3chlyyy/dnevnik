a76a7a564c780f24e882027788696f71bc127bde
w1t3chlyyy <w1t3chlyyy@mail.ru>
2026-09-14 18:59:51 +0300

    Update TelegramInit.js

--- components/TelegramInit.js
+++ components/TelegramInit.js
@@ -3,16 +3,11 @@ import { useEffect } from "react";
 
 export default function TelegramInit() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://telegram.org/js/telegram-web-app.js";
-    script.onload = () => {
-      const webApp = window.Telegram?.WebApp;
-      webApp?.ready();
-      webApp?.expand();
-      webApp?.setHeaderColor("#08080a");
-      webApp?.setBackgroundColor("#08080a");
-    };
-    document.head.appendChild(script);
+    const webApp = window.Telegram?.WebApp;
+    webApp?.ready();
+    webApp?.expand();
+    webApp?.setHeaderColor("#08080a");
+    webApp?.setBackgroundColor("#08080a");
   }, []);
   return null;
 }
