7d5ec6e37a4c3edcab6c00a83aff4a413e6376a0
w1t3chlyyy <w1t3chlyyy@mail.ru>
2026-09-14 18:59:29 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import BottomNav from "@/components/BottomNav";
 import TelegramInit from "@/components/TelegramInit";
 import AmbientBackground from "@/components/AmbientBackground";
@@ -9,6 +10,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ru">
       <body className="max-w-md mx-auto min-h-screen pb-28">
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
         <AmbientBackground />
         <TelegramInit />
         <main className="px-4 pt-6">{children}</main>
